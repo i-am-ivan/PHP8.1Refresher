b2863cf938138e1885f95124fdc29a5219288bcd
i-am-ivan <jovicorp.studio@gmail.com>
2024-11-20 12:34:44 +0300

    1.1.2 Variable + Concatenation + Arrays

--- ByLaracast/index.php
+++ ByLaracast/index.php
@@ -3,6 +3,9 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="styles.css">
+
         <title> Demo </title>
     </head>
 
@@ -10,11 +13,48 @@
 
         <?php
             # Variables
-
+            ## Strings
             $greetings = "Welcome";
+            $name = "Dark Matter";
 
-            echo "<h1> $greetings </h1> 1.1"; # Pass variable + concatenation
+            ## Boolean
+            $read = true;
+
+            ## Arrays
+            $books = [
+                "Religious",
+                "Politics",
+                "Fantasy",
+                "Fiction",
+                "Songs"
+            ];
+
+            # echo "<h1> $greetings!</h1> 1.1"; # Pass variable + concatenation
         ?>
+
+        <h1>
+            <?php echo "$greetings! $name"; ?>
+        </h1>
+
+        <p>
+            You've read it? <?php
+                            # Conditionals
+                                if($read === true)
+                                    { echo "Yes"; }
+                                else
+                                    { echo "Nope"; }
+                            ?>.
+        </p>
+
+        <h3> What of the following list:</h3>
+
+        <ul>
+            <?php foreach($books as $book) : ?>
+
+                       <li> <?= $book; # Short 'echo' tags ?> </li>
+
+            <?php endforeach; ?>
+        </ul>
 
     </body>
 
