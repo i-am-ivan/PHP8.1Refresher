d2b0bb83a3c009ae2c4b065d785ccc91657d3edd
i-am-ivan <jovicorp.studio@gmail.com>
2024-11-20 11:40:34 +0300

    1. Setup PHP server on Project directory, mix PHP

---
+++ ByLaracast/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> Demo </title>
+    </head>
+
+    <body>
+
+        <?php echo "<h1>Demo</h1>"; ?>
+
+    </body>
+
+</html>
